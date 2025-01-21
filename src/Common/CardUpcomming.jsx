16661f120b292734487d7856793bdbff095dbb10
sha2311 <sha31417@gmail.com>
2025-01-22 02:09:05 +0600

    res fix

--- src/Common/CardUpcomming.jsx
+++ src/Common/CardUpcomming.jsx
@@ -4,7 +4,7 @@ const CardUpcoming = ({heading, all, data}) => {
   
 
   return (
-    <section className="w-full lg:max-w-[525px] h-auto rounded-lg bg-neutral-50/70 border border-[#E2E8F0] shadow-cardShadow  mb-5 lg:mb-0 lg:ms-7 lg:pb-[22px]">
+    <section className="w-full lg:max-w-[525px] h-auto rounded-lg bg-neutral-50/70 border border-[#E2E8F0] shadow-cardShadow   mb-5 lg:mb-0 lg:ms-7 lg:pb-[22px]">
       <div className="flex justify-between items-center mt-6 ps-3 lg:ps-[29px] ">
         <h1 className="text-[19px] text-secondary font-medium opacity-80 leading-[22px] ">{heading} </h1>
         <div className="flex items-center pr-5 gap-4">
@@ -18,7 +18,7 @@ const CardUpcoming = ({heading, all, data}) => {
           <div className="flex items-start gap-4 hover:border-[1.5px] bg-white border-primary  lg:px-5 py-2 rounded-lg lg:ms-7 lg:mr-5">
 
             <img
-              className="bg-gray-100 rounded p-2 w-12 h-12"
+              className="bg-gray-100 rounded p-2 lg:w-12 lg:h-12"
               src={item.img}
               alt={item.title}
             />
